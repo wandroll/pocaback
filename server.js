cf43bbe3c3532aa9a9cbde0d630b5bdc3d60462a
Wandrille Verlut <wandrille.verlut@gmail.com>
2017-01-17 23:49:06 +0100

    Add simplest authentication

--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 * Inspired by https://github.com/scotch-io
 */
 
-var express 	= require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 //var mongoose    = require('mongoose');
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./user'); // get our mongoose model
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+//var config = require('./config'); // get our config file
+//var User = require('./user'); // get our mongoose model
 
 
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 //mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+app.set('superSecret', 'config.secret'); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,15 +27,15 @@ app.use(morgan('dev'));
 // =================================================================
 // routes ==========================================================
 // =================================================================
-app.get('/setup', function(req, res) {
+app.get('/setup', function (req, res) {
 
 	// create a sample user
-	var nick = new User({ 
-		name: 'Nick Cerminara', 
+	var nick = new User({
+		name: 'Nick Cerminara',
 		password: 'password',
-		admin: true 
+		admin: true
 	});
-	nick.save(function(err) {
+	nick.save(function (err) {
 		if (err) throw err;
 
 		console.log('User saved successfully');
@@ -44,59 +44,69 @@ app.get('/setup', function(req, res) {
 });
 
 // basic route (http://localhost:8080)
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
 // ---------------------------------------------------------
 // get an instance of the router for api routes
 // ---------------------------------------------------------
-var apiRoutes = express.Router(); 
+var apiRoutes = express.Router();
+
+
 
 // ---------------------------------------------------------
 // authentication (no middleware necessary since this isnt authenticated)
 // ---------------------------------------------------------
 // http://localhost:8080/api/authenticate
-apiRoutes.post('/authenticate', function(req, res) {
+apiRoutes.post('/authenticate', function (req, res) {
 
-	// find the user
-	User.findOne({
-		name: req.body.name
-	}, function(err, user) {
 
-		if (err) throw err;
+	var user = {
+		name: 'usr',
+		password: 'pwd'
+	};
 
-		if (!user) {
-			res.json({ success: false, message: 'Authentication failed. User not found.' });
-		} else if (user) {
+	/*	// find the user
+	
+		
+		User.findOne({
+			name: req.body.name
+		}, function(err, user) {
+	
+			if (err) throw err;
+	*/
+	if (user) {
+		res.json({ success: false, message: 'Authentication failed' });
+	} else if (user) {
 
-			// check if password matches
-			if (user.password != req.body.password) {
-				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-			} else {
+		// check if password matches
+		if (user.name != req.body.name && user.password != req.body.password) {
+			res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+		} else {
 
-				// if user is found and password is right
-				// create a token
-				var token = jwt.sign(user, app.get('superSecret'), {
-					expiresIn: 86400 // expires in 24 hours
-				});
+			// if user is found and password is right
+			// create a token
+			var token = jwt.sign(user, app.get('superSecret'), {
+				expiresIn: 86400 // expires in 24 hours
+			});
 
-				res.json({
-					success: true,
-					message: 'Enjoy your token!',
-					token: token
-				});
-			}		
-
+			res.json({
+				success: true,
+				message: 'Enjoy your token!',
+				token: token
+			});
 		}
 
-	});
+		/*		}
+		*/
+	};
 });
 
 // ---------------------------------------------------------
 // route middleware to authenticate and check token
 // ---------------------------------------------------------
-apiRoutes.use(function(req, res, next) {
+apiRoutes.use(function (req, res, next) {
 
 	// check header or url parameters or post parameters for token
 	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
@@ -105,12 +115,12 @@ apiRoutes.use(function(req, res, next) {
 	if (token) {
 
 		// verifies secret and checks exp
-		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
+		jwt.verify(token, app.get('superSecret'), function (err, decoded) {
 			if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });		
+				return res.json({ success: false, message: 'Failed to authenticate token.' });
 			} else {
 				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;	
+				req.decoded = decoded;
 				next();
 			}
 		});
@@ -119,29 +129,29 @@ apiRoutes.use(function(req, res, next) {
 
 		// if there is no token
 		// return an error
-		return res.status(403).send({ 
-			success: false, 
+		return res.status(403).send({
+			success: false,
 			message: 'No token provided.'
 		});
-		
+
 	}
-	
+
 });
 
 // ---------------------------------------------------------
 // authenticated routes
 // ---------------------------------------------------------
-apiRoutes.get('/', function(req, res) {
+apiRoutes.get('/', function (req, res) {
 	res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
-apiRoutes.get('/users', function(req, res) {
-	User.find({}, function(err, users) {
+apiRoutes.get('/users', function (req, res) {
+	User.find({}, function (err, users) {
 		res.json(users);
 	});
 });
 
-apiRoutes.get('/check', function(req, res) {
+apiRoutes.get('/check', function (req, res) {
 	res.json(req.decoded);
 });
 
